bcfeb724caae89add93dde088dd67d22b610bd6d
sudijia <sudijia@gmail.com>
2016-11-03 19:27:11 -0700

    Update flow.py

--- flow.py
+++ flow.py
@@ -1,4 +1,12 @@
 class Flow:
+	"""  
+	    -id is the identifier of the flow object;
+	    -source would be node that send outs the packets;
+	    -destination would be the destination of each packets;
+	    -amount would be the data amount in bytes that we send the packets;
+	    -start is the start time of the flow process
+	
+	"""
 	def __init__(self,flowObject):
 		self.id=flowObject['id']
 		self.source=flowObject['source']
